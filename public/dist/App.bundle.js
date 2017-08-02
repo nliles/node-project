53459a8c33a22bf44402b181af83a154140d417a
Natalie_Liles <Natalieliles@gmail.com>
2017-08-02 15:45:06 -0400

    Fix webpack

--- public/dist/App.bundle.js
+++ public/dist/App.bundle.js
@@ -6,9 +6,9 @@
 /******/ 	function __webpack_require__(moduleId) {
 /******/
 /******/ 		// Check if module is in cache
-/******/ 		if(installedModules[moduleId])
+/******/ 		if(installedModules[moduleId]) {
 /******/ 			return installedModules[moduleId].exports;
-/******/
+/******/ 		}
 /******/ 		// Create a new module (and put it into the cache)
 /******/ 		var module = installedModules[moduleId] = {
 /******/ 			i: moduleId,
@@ -32,9 +32,6 @@
 /******/
 /******/ 	// expose the module cache
 /******/ 	__webpack_require__.c = installedModules;
-/******/
-/******/ 	// identity function for calling harmony imports with the correct context
-/******/ 	__webpack_require__.i = function(value) { return value; };
 /******/
 /******/ 	// define getter function for harmony exports
 /******/ 	__webpack_require__.d = function(exports, name, getter) {
@@ -63,45 +60,100 @@
 /******/ 	__webpack_require__.p = "";
 /******/
 /******/ 	// Load entry module and return exports
-/******/ 	return __webpack_require__(__webpack_require__.s = 7);
+/******/ 	return __webpack_require__(__webpack_require__.s = 0);
 /******/ })
 /************************************************************************/
 /******/ ([
 /* 0 */
-/***/ (function(module, __webpack_exports__, __webpack_require__) {
+/***/ (function(module, exports, __webpack_require__) {
 
 "use strict";
-function autocomplete(input, latInput, lngInput) {
-	if(!input) return;
-
-	const dropdown = new google.maps.places.Autocomplete(input);
-
-	$(dropdown).on('place_changed', () => {
-		const place = dropdown.getPlace();
-		latInput.value = place.geometry.location.lat();
-		lngInput.value = place.geometry.location.lng();
-	});
-
-	input.on('keydown', (e) => {
-		if(e.keyCode === 13) e.preventDefault();
-	})
-}
-
-/* harmony default export */ __webpack_exports__["a"] = autocomplete;
 
 
+var _dynamicForm = __webpack_require__(1);
+
+var _dynamicForm2 = _interopRequireDefault(_dynamicForm);
+
+var _bling = __webpack_require__(2);
+
+var _autocomplete = __webpack_require__(3);
+
+var _autocomplete2 = _interopRequireDefault(_autocomplete);
+
+var _loginModal = __webpack_require__(4);
+
+var _loginModal2 = _interopRequireDefault(_loginModal);
+
+var _registerModal = __webpack_require__(5);
+
+var _registerModal2 = _interopRequireDefault(_registerModal);
+
+var _imageSlider = __webpack_require__(6);
+
+var _imageSlider2 = _interopRequireDefault(_imageSlider);
+
+var _dropDown = __webpack_require__(7);
+
+var _dropDown2 = _interopRequireDefault(_dropDown);
+
+var _displayMap = __webpack_require__(8);
+
+var _displayMap2 = _interopRequireDefault(_displayMap);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+(0, _autocomplete2.default)((0, _bling.$)('.address'), (0, _bling.$)('.lat'), (0, _bling.$)('.lng'));
 
 /***/ }),
 /* 1 */
-/***/ (function(module, __webpack_exports__, __webpack_require__) {
+/***/ (function(module, exports, __webpack_require__) {
 
 "use strict";
-/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "a", function() { return $; });
-/* unused harmony export $$ */
+
+
+function dynamicForm(max, containerClass, buttonClass, html) {
+    var wrapper = $(containerClass);
+    var addButton = $(buttonClass);
+
+    var x = 1;
+
+    $(addButton).click(function (e) {
+        e.preventDefault();
+        if (x < max) {
+            x++;
+            $(wrapper).prepend(html + '<a href="#" class="delete">Delete</a></div>');
+        } else {
+            alert("The maximum you can add is " + max + ".");
+        }
+    });
+
+    $(wrapper).on("click", ".delete", function (e) {
+        e.preventDefault();
+        $(this).parent('div').remove();
+        x--;
+    });
+}
+
+var html = '<div><input type="file" name="photo" accept="image/gif, image/png, image/jpeg" multiple/>';
+dynamicForm(5, ".container2", ".addPhotoField", html);
+
+var html1 = '<div><label for="address">Address</label><input type="text" class="address" name="locations[address]"/><label for="lng">Address lng</label><input type="text" class="lng" name="locations[coordinates][0]" required/><label for="lat">Address Lat</label><input type="text" class="lat" name="locations[coordinates][1]" required />';
+dynamicForm(10, ".container1", ".addLocationField", html1);
+
+/***/ }),
+/* 2 */
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
 // based on https://gist.github.com/paulirish/12fb951a8b893a454b32
 
-const $ = document.querySelector.bind(document);
-const $$ = document.querySelectorAll.bind(document);
+var $ = document.querySelector.bind(document);
+var $$ = document.querySelectorAll.bind(document);
 
 Node.prototype.on = window.on = function (name, fn) {
   this.addEventListener(name, fn);
@@ -110,91 +162,105 @@ Node.prototype.on = window.on = function (name, fn) {
 NodeList.prototype.__proto__ = Array.prototype; // eslint-disable-line
 
 NodeList.prototype.on = NodeList.prototype.addEventListener = function (name, fn) {
-  this.forEach((elem) => {
+  this.forEach(function (elem) {
     elem.on(name, fn);
   });
 };
 
-
-
-
-/***/ }),
-/* 2 */
-/***/ (function(module, exports) {
-
-$('.cool > li').mouseenter(
-	function() {
-		$('.dropdownBackground').addClass('open');
-		$(this).addClass('trigger-enter');
-		setTimeout(() => {
-			$(this).addClass('trigger-enter-active');	
-		}, 150)
-
-	  const dropdownCoords = $(this).find('.dropdown').offset();
-
-	$('.dropdownBackground').css("width", `${dropdownCoords.width}px`);
-	$('.dropdownBackground').css("height", `${dropdownCoords.height}px`);
-	$('.dropdownBackground').css("transform", `translate(${dropdownCoords.left}px, ${dropdownCoords.top}px)`);
-})
-
-$('.cool > li').mouseleave(
-	function() {
-		$('.dropdownBackground').removeClass('open');
-		$(this).removeClass('trigger-enter', 'trigger-enter-active');	
-})
-
-
-
-
-
-
-
+exports.$ = $;
+exports.$$ = $$;
 
 /***/ }),
 /* 3 */
-/***/ (function(module, exports) {
+/***/ (function(module, exports, __webpack_require__) {
 
-function dynamicForm(max, containerClass, buttonClass, html) {
-    var maxFields       = max;
-    var wrapper         = $(containerClass);
-    var addButton      = $(buttonClass);
-  
-    var x = 1;
+"use strict";
 
-    $(addButton).click(function(e) {
-        e.preventDefault();
-        if(x < maxFields){
-            x++;
-            $(wrapper).prepend(html + '<a href="#" class="delete">Delete</a></div>' ); 
-        }
-       else {
-            alert('The maximum you can add is 5.')
-        }
-    });
-  
-    $(wrapper).on("click", ".delete", function(e){
-        e.preventDefault(); 
-        $(this).parent('div').remove(); 
-        x--;
-    })
+
+Object.defineProperty(exports, "__esModule", {
+	value: true
+});
+function autocomplete(input, latInput, lngInput) {
+	if (!input) return;
+
+	var dropdown = new google.maps.places.Autocomplete(input);
+
+	$(dropdown).on('place_changed', function () {
+		var place = dropdown.getPlace();
+		latInput.value = place.geometry.location.lat();
+		lngInput.value = place.geometry.location.lng();
+	});
+
+	input.on('keydown', function (e) {
+		if (e.keyCode === 13) e.preventDefault();
+	});
 }
 
-const html = '<div><input type="file" name="photo" accept="image/gif, image/png, image/jpeg" multiple/>'
-dynamicForm(5, ".container2", ".addPhotoField", html)
-
-html1 = '<div><label for="address">Address</label><input type="text" class="address" name="location[address]/><label for="lng">Address lng</label><input type="lng" class="lng" name="location[coordinates][0]" required/><label for="lat">Address Lat</label><input type="lat" class="lat" name="location[coordinates][1]" required />'
-dynamicForm(10, ".container1", ".addLocationField", html1)
-
-
-
-
-
-
+exports.default = autocomplete;
 
 /***/ }),
 /* 4 */
-/***/ (function(module, exports) {
+/***/ (function(module, exports, __webpack_require__) {
 
+"use strict";
+
+
+$('#dialog').dialog({
+         autoOpen: false,
+         width: 600,
+         height: 400,
+         modal: true,
+         resizable: false,
+         draggable: false
+});
+$('.login').click(function (e) {
+         $('#dialog').dialog('open');
+         // $('html').css('overflow', 'hidden');
+         // $('body').bind('touchmove', function(e) {
+         //      e.preventDefault()
+         //  });
+});
+$('#dialog').dialog({
+         close: function close(event, ui) {
+                  $('html').css('overflow', 'scroll');
+                  // $('body').unbind('touchmove');			
+         }
+});
+
+/***/ }),
+/* 5 */
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+
+
+$('#dialog2').dialog({
+         autoOpen: false,
+         width: 550,
+         height: 350,
+         modal: true,
+         resizable: false,
+         draggable: false
+});
+$('.signup').click(function (e) {
+         $('#dialog2').dialog('open');
+         // $('html').css('overflow', 'hidden');
+         // $('body').bind('touchmove', function(e) {
+         //      e.preventDefault()
+         //  });
+});
+$('#dialog2').dialog({
+         close: function close(event, ui) {
+                  $('html').css('overflow', 'scroll');
+                  // $('body').unbind('touchmove');			
+         }
+});
+
+/***/ }),
+/* 6 */
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
 // var sliderCount = 1;
 // showImage(sliderCount);
 
@@ -213,111 +279,40 @@ dynamicForm(10, ".container1", ".addLocationField", html1)
 //     images[sliderCount-1].style.display = "block"; 
 // }
 
-/***/ }),
-/* 5 */
-/***/ (function(module, exports) {
-
-
-    $('#dialog').dialog({
-            autoOpen: false,
-            width: 600,
-            height: 400,
-            modal: true,
-            resizable: false,
-            draggable: false
-    })
-	$('.login').click(function(e) {
-		   $('#dialog').dialog('open'); 
-		   // $('html').css('overflow', 'hidden');
-           // $('body').bind('touchmove', function(e) {
-           //      e.preventDefault()
-           //  });
-	});
-	$('#dialog').dialog({
-   		close: function( event, ui ) {
-            $('html').css('overflow', 'scroll');
-            // $('body').unbind('touchmove');			
-   		}
-	});
- 
-
-
-
-/***/ }),
-/* 6 */
-/***/ (function(module, exports) {
-
-  $('#dialog2').dialog({
-            autoOpen: false,
-            width: 550,
-            height: 350,
-            modal: true,
-            resizable: false,
-            draggable: false
-    })
-	$('.signup').click(function(e) {
-		   $('#dialog2').dialog('open'); 
-		   // $('html').css('overflow', 'hidden');
-           // $('body').bind('touchmove', function(e) {
-           //      e.preventDefault()
-           //  });
-	});
-	$('#dialog2').dialog({
-   		close: function( event, ui ) {
-            $('html').css('overflow', 'scroll');
-            // $('body').unbind('touchmove');			
-   		}
-	});
-
-
-
 
 /***/ }),
 /* 7 */
-/***/ (function(module, __webpack_exports__, __webpack_require__) {
+/***/ (function(module, exports, __webpack_require__) {
 
 "use strict";
-Object.defineProperty(__webpack_exports__, "__esModule", { value: true });
-/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__modules_dropDown__ = __webpack_require__(2);
-/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__modules_dropDown___default = __webpack_require__.n(__WEBPACK_IMPORTED_MODULE_0__modules_dropDown__);
-/* harmony import */ var __WEBPACK_IMPORTED_MODULE_1__modules_dynamicForm__ = __webpack_require__(3);
-/* harmony import */ var __WEBPACK_IMPORTED_MODULE_1__modules_dynamicForm___default = __webpack_require__.n(__WEBPACK_IMPORTED_MODULE_1__modules_dynamicForm__);
-/* harmony import */ var __WEBPACK_IMPORTED_MODULE_2__modules_bling__ = __webpack_require__(1);
-/* harmony import */ var __WEBPACK_IMPORTED_MODULE_3__modules_autocomplete__ = __webpack_require__(0);
-/* harmony import */ var __WEBPACK_IMPORTED_MODULE_4__modules_loginModal__ = __webpack_require__(5);
-/* harmony import */ var __WEBPACK_IMPORTED_MODULE_4__modules_loginModal___default = __webpack_require__.n(__WEBPACK_IMPORTED_MODULE_4__modules_loginModal__);
-/* harmony import */ var __WEBPACK_IMPORTED_MODULE_5__modules_registerModal__ = __webpack_require__(6);
-/* harmony import */ var __WEBPACK_IMPORTED_MODULE_5__modules_registerModal___default = __webpack_require__.n(__WEBPACK_IMPORTED_MODULE_5__modules_registerModal__);
-/* harmony import */ var __WEBPACK_IMPORTED_MODULE_6__modules_imageSlider__ = __webpack_require__(4);
-/* harmony import */ var __WEBPACK_IMPORTED_MODULE_6__modules_imageSlider___default = __webpack_require__.n(__WEBPACK_IMPORTED_MODULE_6__modules_imageSlider__);
-/* harmony import */ var __WEBPACK_IMPORTED_MODULE_7__modules_displayMap__ = __webpack_require__(8);
-/* harmony import */ var __WEBPACK_IMPORTED_MODULE_7__modules_displayMap___default = __webpack_require__.n(__WEBPACK_IMPORTED_MODULE_7__modules_displayMap__);
 
 
+$('.cool > li').mouseenter(function () {
+	var _this = this;
 
+	$('.dropdownBackground').addClass('open');
+	$(this).addClass('trigger-enter');
+	setTimeout(function () {
+		$(_this).addClass('trigger-enter-active');
+	}, 150);
 
+	var dropdownCoords = $(this).find('.dropdown').offset();
 
+	$('.dropdownBackground').css("width", dropdownCoords.width + 'px');
+	$('.dropdownBackground').css("height", dropdownCoords.height + 'px');
+	$('.dropdownBackground').css("transform", 'translate(' + dropdownCoords.left + 'px, ' + dropdownCoords.top + 'px)');
+});
 
-
-
-
-
-
-__webpack_require__.i(__WEBPACK_IMPORTED_MODULE_3__modules_autocomplete__["a" /* default */])( __webpack_require__.i(__WEBPACK_IMPORTED_MODULE_2__modules_bling__["a" /* $ */])('.address'), __webpack_require__.i(__WEBPACK_IMPORTED_MODULE_2__modules_bling__["a" /* $ */])('.lat'), __webpack_require__.i(__WEBPACK_IMPORTED_MODULE_2__modules_bling__["a" /* $ */])('.lng') );
-
-
+$('.cool > li').mouseleave(function () {
+	$('.dropdownBackground').removeClass('open');
+	$(this).removeClass('trigger-enter', 'trigger-enter-active');
+});
 
 /***/ }),
 /* 8 */
-/***/ (function(module, exports) {
+/***/ (function(module, exports, __webpack_require__) {
 
-var map;
-	function initMap() {
-		map = new google.maps.Map(document.getElementById('map'), {
-		  center: {lat: -34.397, lng: 150.644},
-		  zoom: 8
-	});
-}
+"use strict";
 
 
 /***/ })
