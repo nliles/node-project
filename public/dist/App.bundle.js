89e81f8e97e2641d1a5706f799e69864e6d11790
Natalie_Liles <Natalieliles@gmail.com>
2017-08-03 13:01:05 -0400

    Fix imageslider issue.

--- public/dist/App.bundle.js
+++ public/dist/App.bundle.js
@@ -235,35 +235,39 @@ $('#dialog2').dialog({
 /***/ (function(module, exports, __webpack_require__) {
 
 "use strict";
-// $('.display-left').on('click', function() {
-// 	buttonClick(-1)
-// })
-
-// $('.display-right').on('click', function() {
-// 	buttonClick(+1)
-// })
 
 
-// function buttonClick(n) {
-//   showImage(sliderCount += n);
-// }
+$('.display-left').on('click', function () {
+  buttonClick(-1);
+});
 
-// function showImage(n) {
-//   var i;
-//   var images = document.getElementsByClassName("mySlides");
-//   if (n > images.length) {sliderCount = 1}; 
-//   if (n < 1) {sliderCount = images.length};
-//   for (i = 0; i < images.length; i++) {
-//       images[i].style.display = "none"; 
-//   }
-//     images[sliderCount-1].style.display = "block"; 
-// }
-// // if this page has a slider:
+$('.display-right').on('click', function () {
+  buttonClick(+1);
+});
 
+function buttonClick(n) {
+  showImage(sliderCount += n);
+}
 
-// var sliderCount = 1;
-// showImage(sliderCount);  
+function showImage(n) {
+  var i;
+  var images = document.getElementsByClassName("mySlides");
+  if (n > images.length) {
+    sliderCount = 1;
+  };
+  if (n < 1) {
+    sliderCount = images.length;
+  };
+  for (i = 0; i < images.length; i++) {
+    images[i].style.display = "none";
+  }
+  images[sliderCount - 1].style.display = "block";
+}
 
+var sliderCount = 1;
+
+var images = document.getElementsByClassName("mySlides");
+images.length > 0 ? showImage(sliderCount) : "";
 
 /***/ }),
 /* 6 */
